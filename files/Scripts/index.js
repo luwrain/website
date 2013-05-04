c00477d2c53d19e64702c9fad46fccdc2754890f
Michael Pozhidaev <msp@altlinux.ru>
2013-05-05 01:37:17 +0700

    Intermediate commit

---
+++ files/Scripts/index.js
@@ -0,0 +1,43 @@
+function OnReady(langName) {
+	$('#topPart').addClass('hidden');
+	$('#closeMenu').addClass('hidden');
+	$('#openMenu').click(function (e) {
+		e.preventDefault();
+		$('#topPart').slideDown('slow');
+		$('#openMenu').addClass('hidden');
+		$('#closeMenu').removeClass('hidden');
+	});
+	$('#closeMenu').click(function (e) {
+		e.preventDefault();
+		$('#topPart').slideUp('slow');
+		$('#closeMenu').addClass('hidden');
+		$('#openMenu').removeClass('hidden');
+	});
+	$('#closeLanguageMenu').click(function (e) {
+		e.preventDefault();
+		$('#languageMenu').addClass('hidden');
+		$('#languageSelected').removeClass('hidden');
+	});
+	$('#selectedLanguageName').click(function (e) {
+		e.preventDefault();
+		$('#languageMenu').removeClass('hidden');
+		$('#languageSelected').addClass('hidden');
+	});
+	$('#russianLang').click(function (e) {
+		ChangeLanguage('russianLang');
+	});
+	$('#engLang').click(function (e) {
+		ChangeLanguage('engLang');
+	});
+	$('#deutschLang').click(function (e) {
+		ChangeLanguage('deutschLang');
+	});
+	$('#franceLang').click(function (e) {
+		ChangeLanguage('franceLang');
+	});
+};
+function ChangeLanguage(langName) {
+	$('#selectedLanguageName').html($('#'+langName).html());
+	$('#languageSelected').removeClass('hidden');
+	$('#languageMenu').addClass('hidden');
+};
