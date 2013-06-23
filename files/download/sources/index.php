727292f7cdde53bbf990e371969df3129834e97c
Michael Pozhidaev <msp@altlinux.ru>
2013-06-24 02:14:10 +0700

    New pages: - Source code section is added as php-page and as plain text inside of texts/.

---
+++ files/download/sources/index.php
@@ -0,0 +1,47 @@
+<?php include "../../functions.php"; luwrain_begin_page('/download/sources/index.php', 'Sources code');?>
+
+                  <h1>Luwrain source code</h1>
+                  <p>
+                    Luwrain source code is published in&nbsp;form of&nbsp;tarball packages widely popular in&nbsp;the&nbsp;world
+                    of&nbsp;GNU/Linux systems as&nbsp;well as&nbsp;it is&nbsp;accessible in&nbsp;project GIT
+                    repository. Actually there&nbsp;is&nbsp;no big difference which way you should prefer but
+                    with GIT resources you can get more materials and easily follow all updates in&nbsp;the&nbsp;meantime
+                    tarball packages contain only sources of&nbsp;main Luwrain distribution including Luwrain
+                    core, a&nbsp;set of&nbsp;basic applications, etc.
+                  </p>
+                  <p>
+                    All materials are provided under the&nbsp;legal terms described on&nbsp;the&nbsp;<a href="/doc/legal/">corresponding
+                    page</a> of&nbsp;this website.
+                  </p>
+                  <h2>Sources tarball</h2>
+                  <p>
+                    Use the&nbsp;link below to&nbsp;get Luwrain sources packaged in&nbsp;a&nbsp;single tarball.
+                    Please, be&nbsp;aware that This package doesn&apos;t include auxiliary libraries needed
+                    for&nbsp;compilation neither in&nbsp;source nor&nbsp;in&nbsp;binary form.
+                  </p>
+                  <ul>
+                    <li><a href="ftp://ftp.altlinux.org/pub/people/msp/src/luwrain/0.x.x/luwrain-0.1.10.tar.gz">luwrain-0.1.10.tar.gz</a></li>
+                  </ul>
+                  <h2>Luwrain GIT repositories</h2>
+                  <p>
+                    All Luwrain GIT repositories are hosted on&nbsp;<a target="_blank" href="http://github.com/luwrain/">github.com</a>
+                    service. These repositories are:
+                  </p>
+                  <ul>
+                    <li>
+                      <a target="_blank" href="http://github.com/luwrain/luwrain.git">http://github.com/luwrain/luwrain.git</a>:
+                      main Luwrain GIT repository.
+                    </li>
+                    <li>
+                      <a target="_blank" href="http://github.com/luwrain/luwrain-website.git">http://github.com/luwrain/luwrain-website
+                      .git</a>: the&nbsp;GIT repository with all&nbsp;materials of&nbsp;project website.
+                    </li>
+                    <li>
+                      <a target="_blank" href="http://github.com/luwrain/voiceman.git">http://github.com/luwrain/voiceman.git</a>:
+                      the&nbsp;GIT repository with speech server VoiceMan used in&nbsp;the&nbsp;Luwrain distribution
+                      for&nbsp;real-time speech output.
+                    </li>
+                  </ul>
+
+
+<?php luwrain_end_page();?>
