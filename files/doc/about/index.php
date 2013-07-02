70c993f902b59a9745e76bd0e813947109ef7298
Michael Pozhidaev <msp@altlinux.ru>
2013-07-02 14:19:43 +0700

    Intermediate commit: - First format script is added; - Proper names for files inside of texts/ directory; - Fixes for doc/about/.

--- files/doc/about/index.php
+++ files/doc/about/index.php
@@ -1,27 +1,28 @@
-<?php include "../../functions.php"; luwrain_begin_page('/doc/index.php', luwrain_current_lang() == 'ru'?'О проекте':'About the project');?>
+<?php include "../../functions.php"; luwrain_begin_page('/doc/about/index.php', luwrain_current_lang() == 'ru'?'О проекте':'About the project');?>
 
                   <h1>About the project</h1>
                   <h2>What are we working on?</h2>
                   <p>
-                    Luwrain is an operating system trying to apply some new ideas and any available experience
-                    in area of OS design for needs of blind persons. We guess it is possible to precisely describe
-                    the project philosophy with the following brief statements:
+                    First of all, Luwrain is an operating system with an attempt to apply some new ideas and
+                    any available experience in area of OS design for needs of blind persons. We guess we are
+                    able to precisely describe the project philosophy with the following 10 brief statements:
                   </p>
                   <ol>
-                    <li>It is an OS completely designed for blind persons.</li>
-                    <li>
-                      Its auditory is all users who need it, no matter what experience they have, what country
-                      are they from, what profession are they occupiedd in and so on.
-                    </li>
-                    <li>First of all it must be convenient, fast and clearly understandable.</li>
-                    <li>It uses Linux kernel and other FOSS software.</li>
-                    <li>It aims to be sutable for mobile work as well for desktop computers.</li>
+                    <li>It is an OS completely designed for blind and visually impaired persons.</li>
+                    <li>It must be convenient, fast and clearly understandable.</li>
+                    <li>It has to support various languages and localizations.</li>
+                    <li>It aims to be sutable for desktop computers as well as for mobile work.</li>
+                    <li>It is based on the Linux kernel and other FOSS software.</li>
+                    <li>No any bias towards any particular hardware type.</li>
                     <li>It is non-profitable project.</li>
                     <li>
-                      Everybody is able to participate in its development. but authors want to keep high quality
-                      and urge everybody to respect quality requirements for all incoming patches.
+                      Its auditory is all users who need it. No matter what experience they have, what country
+                      are they from, what profession are they occupied in and so on.
                     </li>
-                    <li>There No any bias towards any exact hardware type.</li>
+                    <li>
+                      Everybody is able to participate in its development. but authors want to keep high quality
+                      and urge everybody to respect accepted quality requirements for all incoming patches.
+                    </li>
                     <li>
                       We believe available experience in GNU/Linux world is enough at least to start this work
                       and gives good base for success.
@@ -29,21 +30,36 @@
                   </ol>
                   <p>
                     Current time to do this work is very fortunate as Android, Tizen and FirefoxOS make it
-                    easy to realize that Linux turned out as wonderful base for interesting devices and
-                    in same time it is available for everybody. It is clear what you should do to create cutting-edge
-                    technology if you have proper understanding of LInux world. We would like to treat that
-                    fact as a good chance for solution of rather difficult problems as we never had before.
+                    is easy to realize that Linux turned out as a wonderful base for popular devices and in
+                    same time it is available for everybody. Once you got proper understanding of LInux world
+                    it becomes clear to you what you should do to create cutting-edge technology. We would
+                    like to treat that fact as a good chance for solution of rather difficult problems as we
+                    never had before.
                   </p>
                   <p>
-                    Meantime a lot of modern gadgets can be considered widely not more than just toys and it
-                    turns out especially obvious amid epoch of financial crisis. People learn to distinguish
-                    the things that they really need and the things servicing them mostly for entertainment.
-                    In case of accessibility technologies any discussions what is it: a irreplaceable tool
-                    or an luxury item, are completely improper. There it is always the former.
+                    Meantime a lot of usual gadgets can be eventually considered by their users as some sort
+                    of modern toys. Users give their up as soon as they become boring. Amid a epoch of financial
+                    crisis people learn to distinguish the things that they really need and the things servicing
+                    them mostly for entertainment. In case of accessibility technologies any similar discussions
+                    what exactly gadgets are: a irreplaceable tool or an luxury item, are completely improper.
+                    There it is always the former because disabled persons are not able to do without them.
                   </p>
                   <h2>But it should be nice</h2>
+                  <p>
+                    By the way we would like to notice that Luwrain does not want to be an ordinary project.
+                    We have to do everything possible to make it interesting and attractive. In pursuit of
+                    that goal
+                  </p>
+                  <p>
+                    Commercial companies always imply one really serious problem: all products they yields
+                    usually die on their itself fault.
+                  </p>
                   <h2>Research part</h2>
                   <h2>By the author</h2>
+
+
+
+
 
 <center>
 <div>
--- files/doc/index.php
+++ files/doc/index.php
@@ -1,34 +1,38 @@
 <?php include "../functions.php"; luwrain_begin_page('/doc/index.php', luwrain_current_lang() == 'ru'?'Документация':'Documentation');?>
 
                   <h1>Documentation</h1>
+
                   <p>
-                    We have documentation sections on&nbsp;this website divided onto several groups. First
-                    of&nbsp;all there are the&nbsp;links to&nbsp;the&nbsp;pages with material covering various
-                    technical details of&nbsp;Luwrain environment. All of&nbsp;them are purposed for real or
-                    coming users as&nbsp;well as&nbsp;for&nbsp;developers of&nbsp;any&nbsp;kind. If you would
-                    like to read developer documentation you may want to read sections about developing of&nbsp;Luwrain
-                    environment itself or about creating new application for&nbsp;it. In&nbsp;addition we have
-                    one&nbsp;more group below dedicated to&nbsp;Luwrain developing process fundamentals.
+                    We have documentation sections on this website divided onto several groups. First of all
+                    there are the links to the pages with material covering various technical details of Luwrain
+                    environment. All of them are purposed for real or coming users as well as for developers
+                    of any kind. If you would like to read developer documentation you may want to read sections
+                    about developing of Luwrain environment itself or about creating new application for it.
+                    In addition we have one more group below dedicated to Luwrain developing process fundamentals.
                   </p>
                   <p>Choose technical documentation section you need:</p>
                   <ul>
-                    <li><a href="users/">Luwrain users documentation</a></li>
-                    <li><a href="devel/">Luwrain developing documentation</a></li>
-                    <li><a href="apps/">Luwrain applications creation documentation</a></li>
+                    <li><a href="<?echo luwrain_link('users/');?>">Luwrain users documentation</a></li>
+                    <li><a href="<?echo luwrain_link('devel/');?>">Luwrain developing documentation</a></li>
+                    <li><a href="<?echo luwrain_link('apps/');?>">Luwrain applications creation documentation</a></li>
                   </ul>
                   <p>
                     Luwrain fundamentals describes its legal status, general design essentials and some collaboration
-                    invitations. You can also read these pages if you would like to&nbsp;get complete project
-                    overview at&nbsp;a&nbsp;glance.
+                    invitations. You can also read these pages if you would like to get complete project overview
+                    at a glance.
                   </p>
                   <ul>
-                    <li>overview/ What is Luwrain and why do we think blind users need it?</a></li>
-                    <li><a href="legal/">Legal notes</a></li>
-                    <li><a href="faq/">Frequently asked questions</a></li>
-                    <li><a href="hardware/">Hardware support policy</a></li>
-                    <li><a href="partners/">Be our partner and let do something together</a></li>
-                    <li><a href="sponsors/">Be our sponsor and support our work</a></li>
+                    <li>
+                      <a href="<?echo luwrain_link('about/');?>">The free story what is Luwrain and why do we think blind users need
+                      it?</a>
+                    </li>
+                    <li><a href="<?echo luwrain_link('legal/');?>">Legal notes</a></li>
+                    <li><a href="<?echo luwrain_link('faq/');?>">Frequently asked questions</a></li>
+                    <li><a href="<?echo luwrain_link('hardware/');?>">Hardware support policy</a></li>
+                    <li><a href="<?echo luwrain_link('partners/');?>">Be our partner and let do something together</a></li>
+                    <li><a href="<?echo luwrain_link('sponsors/');?>">Be our sponsor and support our work</a></li>
                   </ul>
+
 
 
 <?php luwrain_end_page();?>
