66e3c70608290c3bfb88a287ff0f346aa268866b
Michael Pozhidaev <msp@altlinux.ru>
2013-11-03 10:22:21 +0700

    Intermediate commit: - New text for pages (sponsors, partners, about); - Luwrain 2-frames presentations added.

--- files/doc/about/index.php
+++ files/doc/about/index.php
@@ -3,99 +3,92 @@
                   <h1>About the project</h1>
                   <h2>What are we working on?</h2>
                   <p>
-                    First of&#160;all, Luwrain is an&#160;operating system with an attempt to&#160;apply some
-                    new ideas and any available experience in&#160;area of&#160;OS design for&#160;needs of&#160;blind
-                    persons. Current time to do this work is very fortunate as Android, Tizen and FirefoxOS
-                    made it easy to&#160;realize that Linux turned&#160;out as&#160;a&#160;wonderful base for&#160;popular
-                    devices and in&#160;same time it&#160;remains publicly available for&#160;using by&#160;everybody.
-                    Once you got proper understanding of&#160;LInux world you get a&#160;clew what you should
-                    do if you&#x27;d like to&#160;create cutting-edge technology. We&#x27;d like to&#160;treat
-                    that fact as&#160;a&#160;good chance for&#160;solution of&#160;rather difficult problems
-                    in&#160;area of&#160;assistive technologies as we never had before.
+                    First of&#160;all, Luwrain is a&#160;specialized OS and a&#160;place to&#160;apply any
+                    new ideas and experience. Android, Tizen and FirefoxOS show that Linux turned&#160;out
+                    as&#160;a&#160;wonderful base for&#160;popular devices, so current time to do this work
+                    is very fortunate. Once you get a&#160;proper understanding of&#160;LInux world you get
+                    a&#160;clew what you should do if you&#x27;d like to&#160;create new&#160;cutting-edge
+                    technology. We sure it&#160;is a&#160;chance as&#160;we never had before to&#160;solve
+                    rather difficult problems in&#160;area of&#160;assistive technologies.
                   </p>
                   <p>
-                    Meantime one important thing should be clarified especially detailly. a&#160;lot of&#160;usual
-                    gadgets based on popular mobile OS like Android or something like that very often eventually
-                    are&#160;considered by&#160;their users as&#160;not&#160;more valuable than some sort of&#160;modern
-                    toys. In case of assistive technologies any doubts what exactly devices&#160;are: an&#160;irreplaceable
+                    Meantime, one important thing should have additional clarification. a&#160;lot of&#160;usual
+                    gadgets often eventually considered as&#160;not&#160;more valuable as&#160;toys. In case
+                    of assistive technologies any doubts what exactly devices&#160;are, an&#160;irreplaceable
                     tool or an&#160;item for&#160;entertainment, are completely improper. They are always the&#160;former
-                    because disabled persons are not&#160;able to&#160;do&#160;without them. Such helpful things
-                    are not able to become boring and users will not give&#160;up them.
+                    as disabled persons are not&#160;able to&#160;do&#160;without them.
                   </p>
-                  <p>Finally, we guess we&#160;are able to&#160;describe the&#160;project philosophy precisely with the&#160;following 10 brief statements:</p>
+                  <p>We gathered our&#160;project philosophy in&#160;following ten brief statements:</p>
                   <ol>
-                    <li>It is an OS completely designed for blind and visually impaired persons.</li>
-                    <li>It must be convenient, fast and clearly understandable.</li>
-                    <li>It&#160;has&#160;to support various languages and localizations.</li>
-                    <li>It aims to be&#160;sutable for&#160;desktop computers as&#160;well&#160;as for mobile work.</li>
-                    <li>It is&#160;based on&#160;the&#160;Linux kernel and other FOSS software.</li>
-                    <li>No any bias towards any particular hardware type.</li>
-                    <li>It&#160;is non-profitable project.</li>
+                    <li>It is an OS completely designed for blind and visually impaired persons</li>
+                    <li>It must be convenient, fast and clearly understandable</li>
+                    <li>It&#160;has&#160;to support various languages and localizations</li>
+                    <li>It aims to be&#160;sutable for&#160;desktop computers as&#160;well&#160;as for mobile devices</li>
+                    <li>It is&#160;based on&#160;the&#160;Linux kernel and other FOSS software</li>
+                    <li>No bias towards any particular hardware type</li>
+                    <li>It&#160;is a&#160;non-profitable project</li>
                     <li>
-                      Its auditory is all users who need it. No&#160;matter what experience they got, what country
-                      are they from, what profession are they occupied&#160;in and so&#160;on.
+                      Its auditory is all users who need it; no&#160;matter what experience they got, what country
+                      are they from, what profession are they occupied&#160;in and so&#160;on
                     </li>
                     <li>
-                      Everybody is&#160;able to&#160;participate in&#160;its development. but authors want to&#160;keep
-                      high quality and urge everybody to&#160;respect accepted quality requirements for&#160;all
-                      incoming patches.
+                      Everybody is&#160;able to&#160;participate in&#160;its development, but authors urge everybody
+                      to&#160;respect accepted quality requirements for&#160;all incoming patches
                     </li>
                     <li>
                       We believe available experience in&#160;GNU/Linux world is&#160;enough at&#160;least to&#160;start
-                      this work and gives good base for&#160;success.
+                      this work and gives good base for&#160;success
                     </li>
                   </ol>
                   <p>
-                    The&#160;following additional sections of&#160;this website can be also useful for&#160;overhaul
+                    The&#160;following additional sections of&#160;this website can be useful for&#160;overhaul
                     understanding what Luwrain is:
                   </p>
                   <ul>
-                    <li><a href="<?echo luwrain_link('/doc/devel/design');?>">Exact technical design details of the&#160;Luwrain distribution.</a></li>
+                    <li><a href="<?echo luwrain_link('/doc/devel/design');?>">Exact technical design details of the&#160;Luwrain distribution</a></li>
                     <li><a href="<?echo luwrain_link('/doc/authors/');?>">Who are the&#160;authors of&#160;the&#160;Luwrain project?</a></li>
                   </ul>
                   <h2>But it should be nice!</h2>
                   <p>
                     By&#160;the&#160;way we would like to notice that Luwrain does not want to&#160;be&#160;an&#160;ordinary
-                    project. We have&#160;to&#160;do&#160;everything possible to&#160;make it&#160;interesting
+                    project. We have&#160;to&#160;do&#160;everything possible to&#160;make it&#160;intriguing
                     and attractive. In&#160;pursuit of&#160;that aim any&#160;ideas how&#160;to&#160;improve
                     user impressions, how&#160;to&#160;make usage easier or how&#160;to&#160;eliminate one
-                    more boring and time consuming thing will be definitely appreciated here. If once Luwrain
-                    users fill themselves using something that is not a&#160;accessibility technology anymore
-                    we&#x27;d like to&#160;treat such moment as&#160;a&#160;real success making Luwrain itself
-                    something very close to a&#160;state of&#160;the&#160;art.
+                    more boring and time consuming thing would be definitely appreciated here. If Luwrain consumers
+                    once would be&#160;able to fill themselves using something that actually is not a&#160;accessibility
+                    technology, we could treat this moment as&#160;a&#160;real success making Luwrain itself
+                    very close to a&#160;state of&#160;the&#160;art.
                   </p>
                   <p>
-                    Luwrain is specific in&#160;this sense as&#160;it has such intention being not a&#160;commercial
-                    product. That has corresponding explanation: commercial companies always are not free from
-                    the&#160;one serious problem: all products they originate usually die at&#160;the&#160;moment
+                    Luwrain is rather specific in&#160;this sense as&#160;it has such intention being not a&#160;commercial
+                    product and that has corresponding explanation: commercial companies always are not free
+                    from the&#160;one serious problem: all products they originate usually die at&#160;the&#160;moment
                     of their fault or at&#160;the moment when their products do not yield gain any more. It
-                    is a&#160;really serious problem because in&#160;these circumstances a&#160;lot of users
+                    is a&#160;really serious problem because with&#160;these circumstances a&#160;lot of users
                     can be left without a&#160;tool they really need.
                   </p>
                   <h2>Research part</h2>
                   <p>
                     Another important part of&#160;Luwrain idea is an&#160;acceptance of&#160;results given
-                    by&#160;modern scientific research in&#160;relevant areas if there are any. The&#160;world
-                    of&#160;information technologies is not frozen. It&#160;changes permanently and Luwrain
-                    should reflect such changes properly. So everybody can fill himself completely free to&#160;propose
-                    new ideas to&#160;Luwrain for&#160;experimental implementation if that does not violate
-                    ten terms stated at&#160;the&#160;top of this page.
+                    by&#160;modern scientific research in&#160;relevant areas. The&#160;world of&#160;information
+                    technologies is not frozen, it&#160;changes permanently and Luwrain should reflect that
+                    properly. Thus everybody can fill himself completely free to&#160;propose new ideas if
+                    that does not violate ten terms stated above.
                   </p>
                   <h2>By the author</h2>
                   <p>
-                    At&#160;the end of&#160;this free story what Luwrain is I&#x27;d like to write a&#160;few
-                    words as&#160;some kind of&#160;personal notes. I have&#160;to use screen reading software
+                    At&#160;the end of&#160;this free story what Luwrain is, I&#x27;d like to write a&#160;few
+                    words as&#160;some kind of&#160;personal notes. I had&#160;to use screen reading software
                     since 2001 and never from that moment was satisfied by&#160;tools I&#160;got. The&#160;closest
                     package to&#160;my&#160;understanding what is the&#160;most suitable software for&#160;blind
-                    users is <a target="_blank" href="http://emacspeak.sourceforge.net">emacspeak</a>. But
+                    users is <a target="_blank" href="http://emacspeak.sourceforge.net">emacspeak</a>, but
                     none of&#160;my previous attempts to&#160;help other people improve their lifes using emacspeak
-                    was successful. After precise analysis what are the&#160;problems Luwrain was created and
-                    all of&#160;Luwrain ideas are exactly conclusions of&#160;my&#160;analysis. Anyway I am
+                    was successful. After precise analysis what the&#160;problems are, Luwrain has been created
+                    and allits ideas are exactly the&#160;conclusions of&#160;my&#160;analysis. Anyway, I am
                     very grateful to&#160;all emacspeak authors because the&#160;results of&#160;their work
-                    give me reliable tool and a&#160;lot of&#160;ideas!
+                    gave me reliable tool and a&#160;lot of&#160;useful thoughts!
                   </p>
                   <p>With the&#160;warmest wishes, Michael Pozhidaev.</p>
-
 
 <center>
 <div>
--- files/doc/sponsors/index.php
+++ files/doc/sponsors/index.php
@@ -2,60 +2,46 @@
 
                   <h1>Financial support</h1>
                   <p>
-                    The&#160;Luwrain project itself is non-profitable and for&#160;successful &#160; evolution
-                    it&#160;needs for&#160;external financial help. Any funding can make life better for&#160;large
-                    groups of&#160;wanting people as&#160;Luwrain pursuits a&#160;significant number of&#160;social
-                    goals. There&#160;are two types of&#160;things Luwrain seeks money donations for:
+                    The&#160;Luwrain project is non-profitable and for&#160;successful &#160; evolution it&#160;needs
+                    for&#160;external financial help. Luwrain takes social aims and any&#160;funding could
+                    make the&#160;life better for&#160;large groups of&#160;wanting people. There&#160;are
+                    several types of&#160;things Luwrain seeks money donations for:
                   </p>
                   <ol>
+                    <li>To&#160;employ additional stuff to&#160;lift development speed</li>
                     <li>
-                      Various types of&#160;hardware including embedded systems and mobile computers to test
-                      Luwrain compatibility with and develop new approaches to&#160;interaction for&#160;blind
-                      people.
+                      To&#160;acquire hardware of&#160;various types to test Luwrain compatibility with, including
+                      embedded systems and mobile computers
                     </li>
-                    <li>Participation in related conferences and exhibitions over the&#160;world.</li>
+                    <li>To&#160;participate in&#160;related conferences and exhibitions over the&#160;world</li>
                   </ol>
                   <h2>New hardware for Luwrain</h2>
                   <p>
                     Linux kernel and Java, which are two&#160;major components of&#160;Luwrain, got good chances
                     for&#160;portability across different architectures. Although general development process
-                    is&#160;performed on usual x86 machines in&#160;most cases prepared environment can be
-                    launched also on&#160;other types of&#160;hardware. The&#160;most interesting devices for&#160;our
-                    tasks are various ARM-computers including embedded systems and mobile gadgets. Both of&#160;them
-                    are very important since they are cheap and quite suitable for&#160;purposes Luwrain is
-                    created for.
-                  </p>
-                  <p>
-                    Meantime general user interface conception taken in&#160;Luwrain has some valuable perspectives
-                    to be&#160;adapted for&#160;other types of&#160;user interaction in&#160;addition to&#160;ideas
-                    considered for&#160;implementation initially. We are&#160;talking mostly about tablet computers
-                    which have some noticeable advantages although in&#160;global sense they can not be taken
-                    as&#160;most suitable PCs for&#160;blind users. Simplicity of&#160;Luwrain objects and
-                    rules probably is&#160;able to&#160;make tablets more useful for&#160;blind persons than
-                    they are now.
-                  </p>
-                  <p>
-                    All of&#160;these tasks require some research and hardware accessible for&#160;developers
-                    for&#160;experiments. In&#160;addition usual popular mobile laptops can be still useful
-                    for&#160;testing and building lists of&#160;exact models compatible with Luwrain releases
-                    according to&#160;acquired experience conclusions.
+                    is&#160;performed on usual x86 machines, the&#160;compiled environment could be likely
+                    launched on&#160;other types of&#160;equipment. The&#160;most interesting hardware is various
+                    ARM-computers including embedded systems and mobile gadgets. Both of&#160;them are very
+                    important since they are cheap and quite suitable for&#160;purposes Luwrain is created
+                    for. General Luwrain UI conception has some valuable perspectives to be&#160;adapted for&#160;nearly
+                    any type of&#160;user interaction. We are speaking here mostly about tablet computers which
+                    have some noticeable advantages although in&#160;global sense they are&#160;not considered
+                    as&#160;most suitable PCs for&#160;blind users.
                   </p>
                   <h2>Conferences and exhibitions</h2>
                   <p>
                     Luwrain ideas are&#160;not restricted by&#160;borders of&#160;any particular country and
                     everybody welcomed <a href="<?echo luwrain_link('/doc/partners/');?>">for&#160;cooperation</a>. We believe
-                    that there are people sharing with us our aspiration and convinced of&#160;same ideas as
-                    we do in&#160;every part of&#160;the&#160;world. We&#x27;d like to&#160;find each other
-                    but in&#160;most cases conferences and exhibitions usually used for&#160;new contacts
-                    establishing remains still inaccessible whereas they require significant amount of&#160;money
-                    for&#160;travel and participation. Any&#160;help making that possible would be&#160;definitely
-                    appreciated!
+                    that in&#160;every part of&#160;the&#160;world there are people sharing with us our aspiration
+                    and convinced in&#160;same ideas as we do. usually the&#160;conferences and exhibitions
+                    are&#160;used to&#160;find each other but whereas they require significant amount of&#160;money
+                    for&#160;travel and participation they remains still inaccessible for us.
                   </p>
                   <p>
-                    If you have any financing offerings for&#160;our project you can use any option from our
-                    <a href="<?echo luwrain_link('/doc/contacts/');?>">contacts</a> page to&#160;discuss them with us. We will
-                    wait your help!
+                    If you have any financing offerings for&#160;our project you can take a&#160;look at&#160;<a href="/luwrain-en.pdf">our
+                    2-frames presentation</a> for&#160;potential sponsors and partners. If you have any questions
+                    we&#160;are always in&#160;touch by&#160;this e-mail: luwrain<i>(&#160;at&#160;)</i>gmail<i>(&#160;dot&#160;)</i>com.
                   </p>
-
+                  <p>Thank you so much!</p>
 
 <?php luwrain_end_page('/doc/sponsors/index.php');?>
