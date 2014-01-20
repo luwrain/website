4b6ee34de02a4f15eb93198683867ae7e5f7a9a9
Michael Pozhidaev <msp@altlinux.ru>
2014-01-20 11:36:51 +0700

    "Not written yet" pages: - API; - community/mailing-list/ -  community/bugs/;

---
+++ files/community/bugs/index.php
@@ -0,0 +1,3 @@
+<?php include "../../functions.php"; luwrain_begin_page('/community/bugs/index.php', luwrain_current_lang() == 'ru'?'Сообщить об ошибке':'Report a bug');?>
+<?php luwrain_content_not_written();?>
+<?php luwrain_end_page('/community/bugs/index.php');?>
