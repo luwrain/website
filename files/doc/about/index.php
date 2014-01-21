4f00d8566020c1544eb0b9a210210f6c615cbe45
Michael Pozhidaev <msp@altlinux.ru>
2014-01-21 11:45:36 +0700

    The new text fixes: - The English version of doc/about/ is deeply edited.

--- files/doc/about/index.php
+++ files/doc/about/index.php
@@ -1,22 +1,18 @@
 <?php include "../../functions.php"; luwrain_begin_page('/doc/about/index.php', luwrain_current_lang() == 'ru'?'О проекте':'About the project');?>
-
                   <h1>About the project</h1>
                   <h2>What are we working on?</h2>
                   <p>
-                    First of&#160;all, Luwrain is a&#160;specialized OS and a&#160;place to&#160;apply any
-                    new ideas and experience. Android, Tizen and FirefoxOS show that Linux turned&#160;out
-                    as&#160;a&#160;wonderful base for&#160;popular devices, so current time to do this work
-                    is very fortunate. Once you get a&#160;proper understanding of&#160;LInux world you get
-                    a&#160;clew what you should do if you&#x27;d like to&#160;create new&#160;cutting-edge
-                    technology. We sure it&#160;is a&#160;chance as&#160;we never had before to&#160;solve
-                    rather difficult problems in&#160;area of&#160;assistive technologies.
-                  </p>
-                  <p>
-                    Meantime, one important thing should have additional clarification. a&#160;lot of&#160;usual
-                    gadgets often eventually considered as&#160;not&#160;more valuable as&#160;toys. In case
-                    of assistive technologies any doubts what exactly devices&#160;are, an&#160;irreplaceable
-                    tool or an&#160;item for&#160;entertainment, are completely improper. They are always the&#160;former
-                    as disabled persons are not&#160;able to&#160;do&#160;without them.
+                    First of&#160;all, Luwrain is a&#160;Linux-based specialized OS and a&#160;place to&#160;apply
+                    any new ideas and experience. Linux grew&#160;up from a&#160;amateurish experiment to&#160;a&#160;serious
+                    platform, suitable for&#160;creating cutting-edge technologies. Android, Tizen and FirefoxOS
+                    nicely proved that it&#160;is really so. Thinking about their success, we&#x27;d like to&#160;consider
+                    the&#160;current time as&#160;a&#160;very fortunate moment for&#160;an&#160;attempt to&#160;solve
+                    various problems in&#160;area of&#160;assistive technologies. One important thing gives
+                    there a&#160;significant difference between our work and the&#160;platforms mentioned above.
+                    The&#160;usual gadgets, unfortunately, are sometimes considered as&#160;curious toys, not&#160;more,
+                    while there are no&#160;doubts whether the&#160;devices with&#160;accessible OS are an&#160;irreplaceable
+                    tool or an&#160;item just for&#160;entertainment. They are always the&#160;former as disabled
+                    persons are not&#160;able to&#160;do&#160;without them.
                   </p>
                   <p>We gathered our&#160;project philosophy in&#160;following ten brief statements:</p>
                   <ol>
@@ -33,46 +29,45 @@
                     </li>
                     <li>
                       Everybody is&#160;able to&#160;participate in&#160;its development, but authors urge everybody
-                      to&#160;respect accepted quality requirements for&#160;all incoming patches
+                      to&#160;respect the&#160;accepted quality requirements for&#160;all incoming patches
                     </li>
                     <li>
                       We believe available experience in&#160;GNU/Linux world is&#160;enough at&#160;least to&#160;start
-                      this work and gives good base for&#160;success
+                      this work
                     </li>
                   </ol>
                   <p>
-                    The&#160;following additional sections of&#160;this website can be useful for&#160;overhaul
-                    understanding what Luwrain is:
+                    The&#160;following additional sections are useful for&#160;overhaul understanding what
+                    Luwrain is:
                   </p>
                   <ul>
-                    <li><a href="<?echo luwrain_link('/doc/devel/design');?>">Exact technical design details of the&#160;Luwrain distribution</a></li>
-                    <li><a href="<?echo luwrain_link('/doc/authors/');?>">Who are the&#160;authors of&#160;the&#160;Luwrain project?</a></li>
+                    <li><a href="<?echo luwrain_link('/doc/difference/');?>">Why is Luwrain different than other accessible solutions?</a></li>
+                    <li><a href="<?echo luwrain_link('/doc/authors/');?>">Who are the&#160;authors of&#160;the&#160;Luwrain?</a></li>
                   </ul>
                   <h2>But it should be nice!</h2>
                   <p>
-                    By&#160;the&#160;way we would like to notice that Luwrain does not want to&#160;be&#160;an&#160;ordinary
+                    By&#160;the&#160;way we would like to notice that Luwrain does not want to&#160;be&#160;an&#160;prosaic
                     project. We have&#160;to&#160;do&#160;everything possible to&#160;make it&#160;intriguing
                     and attractive. In&#160;pursuit of&#160;that aim any&#160;ideas how&#160;to&#160;improve
                     user impressions, how&#160;to&#160;make usage easier or how&#160;to&#160;eliminate one
-                    more boring and time consuming thing would be definitely appreciated here. If Luwrain consumers
-                    once would be&#160;able to fill themselves using something that actually is not a&#160;accessibility
-                    technology, we could treat this moment as&#160;a&#160;real success making Luwrain itself
-                    very close to a&#160;state of&#160;the&#160;art.
+                    more boring and time consuming thing would be definitely appreciated here. The&#160;Luwrain
+                    consumers should forget that they are using any kind of&#160;accessibility technologies.
                   </p>
                   <p>
-                    Luwrain is rather specific in&#160;this sense as&#160;it has such intention being not a&#160;commercial
-                    product and that has corresponding explanation: commercial companies always are not free
-                    from the&#160;one serious problem: all products they originate usually die at&#160;the&#160;moment
-                    of their fault or at&#160;the moment when their products do not yield gain any more. It
-                    is a&#160;really serious problem because with&#160;these circumstances a&#160;lot of users
-                    can be left without a&#160;tool they really need.
+                    Luwrain is rather specific in&#160;this sense as&#160;it has this intention being not a&#160;commercial
+                    product and that has a&#160;corresponding explanation. All&#160;commercial products can
+                    end if the&#160;company, which has originated them, lost an&#160;interest or eventually
+                    realized that it is not so profitable as it wanted initially. It is a&#160;really serious
+                    problem because with&#160;these circumstances a&#160;lot of users can be left without a&#160;tool
+                    they really need. In&#160;case of&#160;opened project everybody is&#160;able to&#160;continue
+                    the&#160;work if&#160;he thinks it&#160;worth it.
                   </p>
                   <h2>Research part</h2>
                   <p>
                     Another important part of&#160;Luwrain idea is an&#160;acceptance of&#160;results given
                     by&#160;modern scientific research in&#160;relevant areas. The&#160;world of&#160;information
                     technologies is not frozen, it&#160;changes permanently and Luwrain should reflect that
-                    properly. Thus everybody can fill himself completely free to&#160;propose new ideas if
+                    properly. Thus everybody can feel himself completely free to&#160;propose new ideas if
                     that does not violate ten terms stated above.
                   </p>
                   <h2>By the author</h2>
@@ -80,13 +75,13 @@
                     At&#160;the end of&#160;this free story what Luwrain is, I&#x27;d like to write a&#160;few
                     words as&#160;some kind of&#160;personal notes. I had&#160;to use screen reading software
                     since 2001 and never from that moment was satisfied by&#160;tools I&#160;got. The&#160;closest
-                    package to&#160;my&#160;understanding what is the&#160;most suitable software for&#160;blind
-                    users is <a target="_blank" href="http://emacspeak.sourceforge.net">emacspeak</a>, but
+                    package to&#160;my&#160;understanding, what is the&#160;most suitable software for&#160;blind
+                    users, is <a target="_blank" href="http://emacspeak.sourceforge.net">emacspeak</a>, but
                     none of&#160;my previous attempts to&#160;help other people improve their lifes using emacspeak
                     was successful. After precise analysis what the&#160;problems are, Luwrain has been created
-                    and allits ideas are exactly the&#160;conclusions of&#160;my&#160;analysis. Anyway, I am
-                    very grateful to&#160;all emacspeak authors because the&#160;results of&#160;their work
-                    gave me reliable tool and a&#160;lot of&#160;useful thoughts!
+                    and all its ideas are exactly the&#160;conclusions of&#160;this&#160;analysis. Anyway,
+                    I am very grateful to&#160;all emacspeak authors because the&#160;results of&#160;their
+                    work gave me reliable tool and a&#160;lot of&#160;useful thoughts!
                   </p>
                   <p>With the&#160;warmest wishes, Michael Pozhidaev.</p>
 
