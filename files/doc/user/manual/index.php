ee2b6c98ad628be7c897b40d400d1da45b2881a8
Michael Pozhidaev <msp@altlinux.ru>
2015-03-13 08:34:58 +0600

    User manual prepared for upload

--- files/doc/user/manual/index.php
+++ files/doc/user/manual/index.php
@@ -1,22 +1,28 @@
 <?php include "../../../functions.php"; luwrain_begin_page('/doc/user/manual/index.php', luwrain_current_lang() == 'ru'?'Руководство пользователя':'User manual');?>
 <?php if (luwrain_current_lang() == 'en') {?>
-                <h1>User manual</h1>
+                <p>Sorry, this page is available in Russian language only.</p>
 
 <?php }?>
 <?php if (luwrain_current_lang() == 'ru') {?>
                 <h1>Руководство пользователя</h1>
                 <p>
-                  Этот раздел документации поможет вам получить представление об&#160;основных понятиях и
-                  правилах работы в&#160;Luwrain. Предполагается, что система уже установлена и готова к&#160;работе
-                  или вы запустили её в&#160;режиме LiveCD, желая ознакомиться без&#160;установки на&#160;жёсткий
-                  диск. Если вам требуются инструкции по&#160;подготовке системы к&#160;работе, вы можете
-                  их&#160;найти <a href="<?php echo luwrain_link('/doc/user/installation/');?>">здесь</a>.
+                  В&#160;нашем руководстве описаны основные принципы работы. Мы&#160;полагаем, что система
+                  уже установлена или вы запустили её в&#160;режиме LiveCD с&#160;целью ознакомиться без&#160;установки
+                  на&#160;жёсткий диск. Если вам требуются инструкции по&#160;подготовке системы к&#160;работе,
+                  вы можете их&#160;найти <a href="<?php echo luwrain_link('/doc/user/installation/');?>">здесь</a>.
                 </p>
                 <p>Выберите интересующий вас раздел:</p>
-                <ul>
-                  <li><a href="<?php echo luwrain_link('essentials/');?>">Основные понятия</a></li>
-                  <li><a href="<?php echo luwrain_link('fundamentals');?>">Основные принципы управления и навигации</a></li>
-                </ul>
+                <ol>
+                  <li><a href="<?php echo luwrain_link('startwork/');?>">Начало работы</a></li>
+                  <li><a href="<?php echo luwrain_link('fundamentals/');?>">Основные принципы навигации</a></li>
+                  <li><a href="<?php echo luwrain_link('switching/');?>">Перемещение между областями</a></li>
+                  <li><a href="<?php echo luwrain_link('clipboard/');?>">Буфер обмена</a></li>
+                </ol>
+                <h2>Работа в&#160;приложениях Luwrain</h2>
+                <ol>
+                  <li><a href="<?php echo luwrain_link('app/commander/');?>">Файловый менеджер</a></li>
+                  <li><a href="<?php echo luwrain_link('app/news/');?>">Чтение новостей</a></li>
+                </ol>
 
 <?php }?>
 <?php luwrain_end_page('/doc/user/manual/index.php');?>
