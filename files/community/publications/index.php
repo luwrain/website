4cd81322d826bfd274703ca6da5bc3abd5a33f7d
Michael Pozhidaev <msp@altlinux.ru>
2015-04-21 23:21:37 +0600

    Seems to be good the publications section (both English and Russian)
    - Proper content of the publications section
    - Full texts should be available on original resources or on download.luwrain.org/pdf/

--- files/community/publications/index.php
+++ files/community/publications/index.php
@@ -1,32 +1,101 @@
 <?php include "../../functions.php"; luwrain_begin_page('/community/publications/index.php', luwrain_current_lang() == 'ru'?'Публикации':'Publications');?>
 <?php if (luwrain_current_lang() == 'en') {?>
                 <h1>Publications</h1>
+                <p>
+                  This&#160; page contains a&#160;list of&#160;the&#160;most interesting and valuable publications
+                  made by&#160;various people about the&#160;project. In&#160;addition, you can find a&#160;collection
+                  of&#160;pdf-files on&#160;<a href="http://download.luwrain.org/pdf/">download.luwrain.org</a>.
+                </p>
+                <h2>Papers</h2>
+                <ul>
+                  <li>
+                    <a href="http://dl.acm.org/citation.cfm?id=2687234&amp;CFID=464939051&amp;CFTOKEN=29213994">The
+                    Framework for Accessible Applications: Text-Based Case for Blind People</a> (ACM&#160;Digital
+                    Library, US, December, 2014, English language)
+                  </li>
+                </ul>
+                <h2>Events</h2>
+                <ul>
+                  <li>
+                    <a href="http://2014.secr.ru/lang/ru/program/submitted-presentations/framework-for-accessible-applications-text-base-case-for-blind-people">Report
+                    on&#160;CEE/SECR</a> (Moscow, Russia, October 23, 2014, Russian language)
+                  <ul>
+                    <li><a href="http://download.luwrain.org/pdf/presentation-ceesecr-ru-2014-10-23.pdf">Presentation</a></li>
+                    <li><a href="https://vimeo.com/118003817">Video recording</a></li>
+                  </ul>
+                  </li>
+                  <li>
+                    <a href="http://download.luwrain.org/pdf/presentation-anmc-en-2014-09-05.pdf">Presentation
+                    for&#160;the&#160;report on&#160;the&#160;summit of&#160;The Asian Network of&#160;Major
+                    Cities</a> (Tomsk, Russia, September 5, 2014, English language)
+                  </li>
+                  <li>
+                    <a href="http://download.luwrain.org/pdf/presentation-LSMRMLL-en-2014-07-09.pdf">Presentation
+                    for&#160;the&#160;report on&#160;LSM/RMLL</a> (Montpellier, France, July 9, 2014, English
+                    language)
+                  </li>
+                </ul>
                 <h2>About the&#160;project</h2>
                 <ul>
                   <li>
-                    <a href="<?php echo luwrain_link('BelyeBukvyNaChernomEkrane/');?>">Белые буквы на&#160;чёрном экране</a> («Кот
-                    Шрёдингера», №1&#x2013;2, Svetlana Sokolova, January&#x2013;February, 2015, Russian language)
+                    <a href="http://kot.sh/statya/127/belye-bukvy-na-chernom-ekrane">Белые буквы на&#160;чёрном
+                    экране</a> («Кот Шрёдингера», Svetlana Sokolova, January&#x2013;February, 2015, Russian
+                    language)
                   </li>
                   <li>
-                    <a target="_blank" href="http://www.youtube.com/watch?v=m-HOWJCRn7g3">Идея фикс. Компьютер
-                    для незрячих</a> («Живое ТВ», Svetlana Glazyrina, Youtube, October 3, 2014, Russian language)
+                    <a href="http://www.youtube.com/watch?v=m-HOWJCRn7g3">Идея фикс. Компьютер для незрячих</a>
+                    (Youtube, «Живое ТВ», Svetlana Glazyrina, October 3, 2014, Russian language)
                   </li>
                 </ul>
 
 <?php }?>
 <?php if (luwrain_current_lang() == 'ru') {?>
                 <h1>Публикации</h1>
+                <p>
+                  На&#160;этой странице приведены наиболее интересные и значимые публикации, сделанные разными
+                  людьми о&#160;Luwrain. Помимо этого, мы выложили некоторые pdf-файлы на&#160;<a href="http://download.luwrain.org/pdf/">download.luwrain.org</a>.
+                </p>
+                <h2>Статьи</h2>
+                <ul>
+                  <li>
+                    <a href="http://dl.acm.org/citation.cfm?id=2687234&amp;CFID=464939051&amp;CFTOKEN=29213994">The
+                    Framework for Accessible Applications: Text-Based Case for Blind People</a> (ACM&#160;Digital
+                    Library, США, декабрь 2014, анг.&#160;яз.)
+                  </li>
+                </ul>
+                <h2>Мероприятия</h2>
+                <ul>
+                  <li>
+                    <a href="http://2014.secr.ru/lang/ru/program/submitted-presentations/framework-for-accessible-applications-text-base-case-for-blind-people">Выступление
+                    на&#160;конференции &#x22;Разработка&#160;ПО &#x2014; 2014&#x22;</a> (Москва, 23 октября
+                    2014&#160;г., рус.&#160;яз.)
+                  <ul>
+                    <li><a href="http://download.luwrain.org/pdf/presentation-ceesecr-ru-2014-10-23.pdf">Презентация</a></li>
+                    <li><a href="https://vimeo.com/118003817">Видеозапись</a></li>
+                  </ul>
+                  </li>
+                  <li>
+                    <a href="http://download.luwrain.org/pdf/presentation-anmc-en-2014-09-05.pdf">Презентация
+                    к&#160;выступлению на&#160;саммите Сети главных городов Азии</a> (Томск, 5 сентября 2014&#160;г.,
+                    анг.&#160;яз.)
+                  </li>
+                  <li>
+                    <a href="http://download.luwrain.org/pdf/presentation-LSMRMLL-en-2014-07-09.pdf">Презентация
+                    к&#160;выступлению на&#160;LSM/RMLL</a> (Монпелье, Франция, 9 июля 2014&#160;г., анг.&#160;яз.)
+                  </li>
+                </ul>
                 <h2>О&#160;проекте</h2>
                 <ul>
                   <li>
-                    <a href="<?php echo luwrain_link('BelyeBukvyNaChernomEkrane/');?>">Белые буквы на&#160;чёрном экране</a> («Кот
-                    Шрёдингера», №1&#x2013;2, Светлана Соколова, январь-февраль 2015&#160;г., рус.&#160;яз.)
+                    <a href="http://kot.sh/statya/127/belye-bukvy-na-chernom-ekrane">Белые буквы на&#160;чёрном
+                    экране</a> («Кот Шрёдингера», Светлана Соколова, январь-февраль 2015&#160;г., рус.&#160;яз.)
                   </li>
                   <li>
-                    <a target="_blank" href="http://www.youtube.com/watch?v=m-HOWJCRn7g3">Идея фикс. Компьютер
-                    для незрячих</a> («Живое ТВ», Светлана Глазырина, Youtube, 3 октября 2014 г., рус.&#160;яз.)
+                    <a href="http://www.youtube.com/watch?v=m-HOWJCRn7g3">Идея фикс. Компьютер для незрячих</a>
+                    (Youtube, «Живое ТВ», Светлана Глазырина,
                   </li>
                 </ul>
+                <p>3 октября 2014 г., рус.&#160;яз.)</p>
 
 <?php }?>
 <?php luwrain_end_page('/community/publications/index.php');?>
