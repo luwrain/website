9c708f488e90ffd56bd6104d7fd95aa20b6d13c7
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-08-07 05:58:46 +0700

    Fixes to images album

---
+++ files/community/album/content.php
@@ -0,0 +1,87 @@
+<?php
+
+  $comments_en = array(	
+    '2013-09-21-conference.jpg' =>
+    'September 21, 2013.',
+
+    '2013-09-21-luwrain-official.jpg' =>
+    'September 21, 2013.',
+
+    '2014-09-05-with-vitek.jpg' =>'',
+    'September 5, 2014.',
+
+    '2014-10-23-report-official1.jpg' =>
+    'October 23, 2014.',
+
+    '2015-06-25-sammy-restaurant.jpg' =>
+    'June 25, 2015.',
+
+    '2015-06-25-wilson-cards.jpg' =>
+    'June 25, 2015.',
+
+    '2015-06-27-hkoscon-report1.jpg' =>
+    'June 27, 2015.',
+
+    '2015-06-27-hkoscon-report2.jpg' =>
+    'June 27, 2015.',
+
+    '2015-10-20-duma-demonstration-to-Smolin.jpg' =>
+    'October 20, 2015.',
+
+    '2015-10-21-lcdr1.jpg' =>
+    'October 21, 2015.',
+
+    '2015-10-23-baumanka-speech1.jpg' =>
+    'October 23, 2015.',
+  );
+
+  $comments_ru = array(	
+    '2013-09-21-conference.jpg' =>
+    '21 сентября 2013 г.',
+
+    '2013-09-21-luwrain-official.jpg' =>
+    '21 сентября 2013 г.',
+
+    '2014-09-05-with-vitek.jpg' =>'',
+    '5 сентября 2014 г.',
+
+    '2014-10-23-report-official1.jpg' =>
+    '23 октября 2014 г.',
+
+    '2015-06-25-sammy-restaurant.jpg' =>
+    '25 июня 2015 г.',
+
+    '2015-06-25-wilson-cards.jpg' =>
+    '25 июня 2015 г.',
+
+    '2015-06-27-hkoscon-report1.jpg' =>
+    '27 июня 2015 г.',
+
+    '2015-06-27-hkoscon-report2.jpg' =>
+    '27 июня 2015 г.',
+
+    '2015-10-20-duma-demonstration-to-Smolin.jpg' =>
+    '20 октября 2015 г.',
+
+    '2015-10-21-lcdr1.jpg' =>
+    '21 октября 2015 г.',
+
+    '2015-10-23-baumanka-speech1.jpg' =>
+    '23 октября 2015 г.',
+  );
+
+  $files = array(
+    '2013-09-21-conference.jpg',
+    '2013-09-21-luwrain-official.jpg',
+    '2014-09-05-with-vitek.jpg',
+    '2014-10-23-report-official1.jpg',
+    '2015-06-25-sammy-restaurant.jpg',
+    '2015-06-25-wilson-cards.jpg',
+    '2015-06-27-hkoscon-report1.jpg',
+    '2015-06-27-hkoscon-report2.jpg',
+    '2015-10-20-duma-demonstration-to-Smolin.jpg',
+    '2015-10-21-lcdr1.jpg',
+    '2015-10-23-baumanka-speech1.jpg',
+  );
+
+?>
--- files/community/album/index.php
+++ files/community/album/index.php
@@ -2,28 +2,32 @@
   include "../../functions.php";
   include "content.php";
   luwrain_begin_page('/community/album/index.php', luwrain_current_lang() == 'ru'?'Наш фотоальбом':'Our pictures');
+  $baseurl = 'http://download.luwrain.org/images/';
   $frame = array_key_exists('id', $_GET)?$_GET['id']:1;
-  $total = count($usual);
+  $total = count($files);
   $index = $total - $frame;
   if ($index < 0)
     $index = 0;
-  $filename = $usual[$index];
-  $comment = $comments[$filename];
+  $filename = $files[$index];
+  if (luwrain_current_lang() == 'ru')
+    $comment = $comments_ru[$filename]; else
+    $comment = $comments_en[$filename];
   echo '      <h1>'.(luwrain_current_lang() == 'ru'?'Наш альбом: фотография ':'Our pictures: image').' '.$frame.(luwrain_current_lang() == 'ru'?' из ':'/').$total."</h1>\n";
 ?>
         <center>
           <p style="font-size: 20px;">
 <?php
   if ($frame > 1)
-    echo '            <a href="index.php?id='.($frame - 1).'">&lt;&lt;</a>'."\n"; else
+    echo '            <a href="index.php?lang='.luwrain_current_lang().'&mode='.luwrain_current_mode().'&id='.($frame - 1).'">&lt;&lt;</a>'."\n"; else
     echo '            &lt;&lt;'."\n";
   if ($frame < $total)
-    echo '            <a href="index.php?id='.($frame + 1).'"><img src="'.$filename.'" hspace="32px" align="middle" alt="'.$comment.'"></a>'."\n"; else
-    echo '            <img src="'.$filename.'" hspace="32px" align="middle" alt="'.$comment.'">'."\n";
+    echo '            <a href="index.php?lang='.luwrain_current_lang().'&mode='.luwrain_current_mode().'&id='.($frame + 1).'"><img src="'.$baseurl.$filename.'" hspace="32px" align="middle" alt="'.$comment.'"></a>'."\n"; else
+    echo '            <img src="'.$baseurl.$filename.'" hspace="32px" align="middle" alt="'.$comment.'">'."\n";
   if ($frame < $total)
-    echo '            <a href="index.php?id='.($frame + 1).'">&gt;&gt;</a>'."\n"; else
+    echo '            <a href="index.php?lang='.luwrain_current_lang().'&mode='.luwrain_current_mode().'&id='.($frame + 1).'">&gt;&gt;</a>'."\n"; else
     echo '            &gt;&gt;'."\n";
 ?>
           </p>
+<?php   echo '          <p><i>'.$comment.'</i></p>'."\n";?>
         </center>
 <?php luwrain_end_page('/community/album/index.php');?>
