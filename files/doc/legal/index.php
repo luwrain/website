3bfe5e16532ed6bc7de37dcf67151e78c7ba8bab
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-09-17 19:26:11 +0700

    doc.legal reformatted in Markdown

--- files/doc/legal/index.php
+++ files/doc/legal/index.php
@@ -1,56 +1,64 @@
 <?php include "../../functions.php"; luwrain_begin_page('/doc/legal/index.php', luwrain_current_lang() == 'ru'?'Правовой статус':'Legal notes');?>
 <?php if (luwrain_current_lang() == 'en') {?>
-                <h1>Legal notes</h1>
-                <p>
-                  The&#160;code of&#160;LUWRAIN core, as&#160;well as&#160;the&#160;most of&#160;its&#160;libraries
-                  and applications, is&#160;distributed under the&#160;terms of&#160;<a href="https://en.wikipedia.org/wiki/GNU_General_Public_License">GNU&#160;General
-                  Public License ver.&#160;3</a>. Any&#160;actions not violating the&#160;requirements of&#160;this
-                  license are&#160;freely permitted and may&#160;be&#160;carried&#160;out without any&#160;prior
-                  permission. LUWRAIN distribution (especially in&#160;a&#160;form of&#160;bootable ISO-image)
-                  contains a&#160;bundle of&#160;various libraries and applications provided by&#160;various
-                  software projects. For&#160;any detailed information about their license agreements please
-                  refer to&#160;original packages, but Usually all of&#160;them permit free commercial and
-                  non-commercial usage. Any software, which prohibits free commercial and non-commercial
-                  usage, may not be shipped with LUWRAIN distribution. Debug releases may have some exceptions
-                  providing components only for&#160;non-commercial purposes.
-                </p>
-                <p>
-                  we would like to&#160;impose some restrictions on&#160;a&#160;name of&#160;our project.
-                  That is&#160;needed to&#160;prevent users confusion by&#160;builds from third-part developers.
-                  If you have proposed some changes in&#160;LUWRAIN core and more than 90% of&#160;source
-                  files remain untouched (meaning, there is any commit in&#160;<a href="https://github.com/luwrain/luwrain.git">the&#160;main
-                  Git repository</a> with more than 90% of same files as in&#160;your custom version) we
-                  encourage you to add some suffix to your release, designating that it&#160;is not an&#160;original
-                  LUWRAIN version, but you still may title your project as&#160;&#x22;LUWRAIN&#x22;. Otherwise,
-                  you may not use LUWRAIN name any&#160;more and must choose any other designation.
-                </p>
-                <p>Thank&#160;you!</p>
+<h1>Legal notes</h1>
 
+<p>The&#160;code published by&#160;members of&#160;the&#160;LUWRAIN Team
+is&#160;distributed under the&#160;terms of&#160;<a href="https://en.wikipedia.org/wiki/GNU_General_Public_License">GNU&#160;General Public License ver.&#160;3</a>.
+Any&#160;actions not violating the&#160;requirements of&#160;this license and not&#160;violating restrictions imposed on&#160;the&#160;name of&#160;the&#160;project are&#160;freely permitted 
+and may&#160;be&#160;carried&#160;out without any&#160;prior permission.
+LUWRAIN distribution (especially in&#160;a&#160;form of&#160;bootable ISO-image)
+contains a&#160;bundle of&#160;various libraries and applications provided by&#160;various software projects.
+For&#160;any detailed information about their license agreements please refer to&#160;original packages,
+but Usually all of&#160;them permit   free commercial and non-commercial usage.
+Any software, which prohibits free commercial and non-commercial usage, may not be shipped with LUWRAIN distribution,
+but we can consider publishing it on&#160;our website.
+Debug releases may have some exceptions providing components only for&#160;non-commercial purposes. </p>
+
+<p>we would like to&#160;impose some restrictions on&#160;a&#160;name of&#160;our project.
+That is&#160;needed to&#160;prevent users confusion by&#160;builds from third-party developers.
+If you have  proposed some changes to&#160;LUWRAIN core,
+you still may title your project as&#160;"LUWRAIN" in&#160;the&#160;case you keep  the&#160;following conditions:</p>
+
+<ol>
+<li>You have&#160;to add some suffix to&#160;your&#160;release, making easy to&#160;understand that it isn't an&#160;official release</li>
+<li>More than 90% of&#160;source files remain untouched 
+(meaning, there is a&#160;commit in&#160;<a href="https://github.com/luwrain/luwrain.git">the&#160;main Git repository</a> with more than 90% of same files as in&#160;your custom version) </li>
+<li>LUWRAIN API remains untouched 
+(meaning, nothing what is covered by <a href="/api/">Javadoc API documentation</a> is changed)</li>
+</ol>
+
+<p>Otherwise, you may not use LUWRAIN name any&#160;more and have&#160;to consider some another designation.
+The&#160;"LUWRAIN" trademark is&#160;applied for&#160;registration on&#160;the&#160;territory of&#160;Russian Federation.</p>
+
+<p>Thank&#160;you!</p>
 <?php }?>
 <?php if (luwrain_current_lang() == 'ru') {?>
-                <h1>Правовой статус</h1>
-                <p>
-                  Все исходные тексты базового пакета LUWRAIN распространяются на&#160;основе GNU&#160;Public
-                  License версии&#160;3. Любое действие с&#160;ними, не&#160;нарушающее требований этой лицензии,
-                  разрешено и может быть выполнено без&#160;предварительного согласия разработчиков. Дистрибутив
-                  LUWRAIN в&#160;виде загрузочного ISO-образа содержит множество приложений от&#160;большого
-                  количества проектов, каждый из&#160;которых может распространяться на&#160;основе собственной
-                  лицензии, но обязательно с&#160;предоставлением свободного права на&#160;коммерческое и
-                  некоммерческое использование. Программное обеспечение, не&#160;предоставляющее такого права,
-                  в&#160;составе дистрибутива LUWRAIN распространяться не&#160;может. Возможны исключения,
-                  в&#160;рамках которых отладочные образы LUWRAIN будут содержать приложения, разрешающие
-                  только некоммерческое использование.
-                </p>
-                <p>
-                  Мы&#160;хотели&#160;бы ограничить право использования названия проекта LUWRAIN. Это&#160;необходимо
-                  для предотвращения введения в&#160;заблуждение пользователей в&#160;случае создания сторонних
-                  сборок LUWRAIN. Если Вы внесли некоторые изменения в&#160;исходный код базового пакета
-                  LUWRAIN, но не&#160;менее 90% файлов остались нетронутыми (в&#160;нашем <a href="<?php echo luwrain_link('/download/sources/');?>">основном
-                  git-репозитории</a> существует коммит, в&#160;котором не&#160;менее 90% файлов совпадают
-                  с&#160;Вашей версией), Вы можете сохранить название LUWRAIN, но с обязательной пометкой,
-                  что это Ваша сторонняя версия. В&#160;противном случае мы просим не&#160;называть разработку
-                  LUWRAIN и выбрать другое имя.
-                </p>
+<h1>Правовой статус</h1>
 
+<p>Все исходные тексты LUWRAIN, созданные членами нашей команды,  распространяются на&#160;основе GNU&#160;Public License версии&#160;3.
+Любое действие с&#160;ними, не&#160;нарушающее требований этой лицензии и не&#160;нарушающее ограничений, наложенных на&#160;имя проекта, разрешено и может быть выполнено без&#160;предварительного согласия разработчиков.
+Дистрибутив LUWRAIN в&#160;виде загрузочного ISO-образа содержит множество компонентов от&#160;большого количества сторонних проектов, 
+каждый из&#160;которых может распространяться на&#160;основе собственной лицензии,
+но обязательно с&#160;предоставлением свободного права на&#160;коммерческое и некоммерческое использование.
+Программное обеспечение, не&#160;предоставляющее такого права, в&#160;составе дистрибутива LUWRAIN распространяться не&#160;может,
+но мы можем опубликовать такие продукты отдельно на&#160;нашем сайте. 
+Возможны исключения, в&#160;рамках которых отладочные образы LUWRAIN будут содержать приложения, разрешающие только некоммерческое использование.</p>
+
+<p>Мы ограничиваем право использования названия "LUWRAIN".
+Это&#160;необходимо для предотвращения введения в&#160;заблуждение пользователей 
+в&#160;случае создания сторонних сборок LUWRAIN.
+Если Вы внесли некоторые изменения в&#160;исходный код базового пакета LUWRAIN,
+Вы можете сохранить название "LUWRAIN" при&#160;соблюдении следующих требований:</p>
+
+<ol>
+<li>Необходимо сделать пометку в&#160;названии, позволяющую определить, что это ваша стороняя версия, которая не&#160;является официальным релизом.</li>
+<li>Не&#160;менее 90% файлов остались нетронутыми
+(в&#160;нашем основном Git-репозитории должен существовать коммит, в&#160;котором не&#160;менее 90% файлов совпадают с&#160;Вашей версией).</li>
+<li>Ваши изменения не&#160;должны влиять на&#160;LUWRAIN&#160;API,
+т.&#160;е. интерфейс, фиксируемой нашей&#160;<a href="/api/">документацией Javadoc для&#160;LUWRAIN&#160;API</a> не&#160;должен измениться.</li>
+</ol>
+
+<p>В&#160;противном случае мы просим не&#160;называть разработку "LUWRAIN" и выбрать другое имя.
+Торговая марка "LUWRAIN" подана на&#160;регистрацию на&#160;территории Российской Федерации.</p>
 <?php }?>
 <?php luwrain_end_page('/doc/legal/index.php');?>
---
+++ texts/doc.legal.md
@@ -0,0 +1,64 @@
+
+ENTITLE='Legal notes'
+RUTITLE='Правовой статус'
+TARGET=doc/legal/index.php
+
+luwrain-page-en
+
+# Legal notes
+
+The~code published by~members of~the~LUWRAIN Team
+is~distributed under the~terms of~[GNU~General Public License ver.~3](https://en.wikipedia.org/wiki/GNU_General_Public_License).
+Any~actions not violating the~requirements of~this license and not~violating restrictions imposed on~the~name of~the~project are~freely permitted 
+and may~be~carried~out without any~prior permission.
+LUWRAIN distribution (especially in~a~form of~bootable ISO-image)
+contains a~bundle of~various libraries and applications provided by~various software projects.
+For~any detailed information about their license agreements please refer to~original packages,
+but Usually all of~them permit   free commercial and non-commercial usage.
+Any software, which prohibits free commercial and non-commercial usage, may not be shipped with LUWRAIN distribution,
+but we can consider publishing it on~our website.
+Debug releases may have some exceptions providing components only for~non-commercial purposes. 
+
+we would like to~impose some restrictions on~a~name of~our project.
+That is~needed to~prevent users confusion by~builds from third-party developers.
+If you have  proposed some changes to~LUWRAIN core,
+you still may title your project as~"LUWRAIN" in~the~case you keep  the~following conditions:
+
+1. You have~to add some suffix to~your~release, making easy to~understand that it isn't an~official release
+2. More than 90% of~source files remain untouched 
+(meaning, there is a~commit in~[the~main Git repository](https://github.com/luwrain/luwrain.git) with more than 90% of same files as in~your custom version) 
+3. LUWRAIN API remains untouched 
+(meaning, nothing what is covered by [Javadoc API documentation](/api/) is changed)
+
+Otherwise, you may not use LUWRAIN name any~more and have~to consider some another designation.
+The~"LUWRAIN" trademark is~applied for~registration on~the~territory of~Russian Federation.
+
+Thank~you!
+
+luwrain-page-ru
+
+# Правовой статус 
+
+Все исходные тексты LUWRAIN, созданные членами нашей команды,  распространяются на~основе GNU~Public License версии~3.
+Любое действие с~ними, не~нарушающее требований этой лицензии и не~нарушающее ограничений, наложенных на~имя проекта, разрешено и может быть выполнено без~предварительного согласия разработчиков.
+Дистрибутив LUWRAIN в~виде загрузочного ISO-образа содержит множество компонентов от~большого количества сторонних проектов, 
+каждый из~которых может распространяться на~основе собственной лицензии,
+но обязательно с~предоставлением свободного права на~коммерческое и некоммерческое использование.
+Программное обеспечение, не~предоставляющее такого права, в~составе дистрибутива LUWRAIN распространяться не~может,
+но мы можем опубликовать такие продукты отдельно на~нашем сайте. 
+Возможны исключения, в~рамках которых отладочные образы LUWRAIN будут содержать приложения, разрешающие только некоммерческое использование.
+
+Мы ограничиваем право использования названия "LUWRAIN".
+Это~необходимо для предотвращения введения в~заблуждение пользователей 
+в~случае создания сторонних сборок LUWRAIN.
+Если Вы внесли некоторые изменения в~исходный код базового пакета LUWRAIN,
+Вы можете сохранить название "LUWRAIN" при~соблюдении следующих требований:
+
+1. Необходимо сделать пометку в~названии, позволяющую определить, что это ваша стороняя версия, которая не~является официальным релизом.
+2. Не~менее 90% файлов остались нетронутыми
+(в~нашем основном Git-репозитории должен существовать коммит, в~котором не~менее 90% файлов совпадают с~Вашей версией).
+3. Ваши изменения не~должны влиять на~LUWRAIN~API,
+т.~е. интерфейс, фиксируемой нашей~[документацией Javadoc для~LUWRAIN~API](/api/) не~должен измениться.
+
+В~противном случае мы просим не~называть разработку "LUWRAIN" и выбрать другое имя.
+Торговая марка "LUWRAIN" подана на~регистрацию на~территории Российской Федерации.
