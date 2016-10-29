aadcddc2df06de921e8d29848e3de2d6f60dded4
StanislauBudzko <stanislau.budzko@gmail.com>
2016-10-29 14:50:42 +0300

    Fixed next image and previous image arrows.

--- files/community/album/index.php
+++ files/community/album/index.php
@@ -15,19 +15,20 @@
   echo '      <h1>'.(luwrain_current_lang() == 'ru'?'Наш альбом: фотография ':'Our pictures: image').' '.$frame.(luwrain_current_lang() == 'ru'?' из ':'/').$total."</h1>\n";
 ?>
         <center>
-          <p style="font-size: 20px;">
-<?php
-  if ($frame > 1)
-    echo '            <a href="index.php?lang='.luwrain_current_lang().'&mode='.luwrain_current_mode().'&id='.($frame - 1).'">&lt;&lt;</a>'."\n"; else
-    echo '            &lt;&lt;'."\n";
-  if ($frame < $total)
-    echo '            <a href="index.php?lang='.luwrain_current_lang().'&mode='.luwrain_current_mode().'&id='.($frame + 1).'"><img src="'.$baseurl.$filename.'" hspace="32px" align="middle" alt="'.$comment.'"></a>'."\n"; else
-    echo '            <img src="'.$baseurl.$filename.'" hspace="32px" align="middle" alt="'.$comment.'">'."\n";
-  if ($frame < $total)
-    echo '            <a href="index.php?lang='.luwrain_current_lang().'&mode='.luwrain_current_mode().'&id='.($frame + 1).'">&gt;&gt;</a>'."\n"; else
-    echo '            &gt;&gt;'."\n";
-?>
-          </p>
+<div class="image-slider">
+  <?php
+    if ($frame > 1)
+      echo '            <a class="photo-prev" href="index.php?lang='.luwrain_current_lang().'&mode='.luwrain_current_mode().'&id='.($frame - 1).'">&lt;&lt;</a>'."\n"; else
+      echo '            &lt;&lt;'."\n";
+    if ($frame < $total)
+      echo '            <a class="image-item" href="index.php?lang='.luwrain_current_lang().'&mode='.luwrain_current_mode().'&id='.($frame + 1).'"><img src="'.$baseurl.$filename.'" hspace="32px" align="middle" alt="'.$comment.'"></a>'."\n"; else
+      echo '            <img src="'.$baseurl.$filename.'" hspace="32px" align="middle" alt="'.$comment.'">'."\n";
+    if ($frame < $total)
+      echo '            <a class="photo-next" href="index.php?lang='.luwrain_current_lang().'&mode='.luwrain_current_mode().'&id='.($frame + 1).'">&gt;&gt;</a>'."\n"; else
+      echo '            &gt;&gt;'."\n";
+  ?>
+  <div class="clearBoth"></div>
+</div>
 <?php   echo '          <p><i>'.$comment.'</i></p>'."\n";?>
         </center>
 <?php luwrain_end_page('/community/album/index.php');?>
