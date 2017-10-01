13b0c540fb9d40968ff3f34fe2a2297687d230ee
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2017-10-01 14:15:24 +0700

    No licrict support any nore

--- files/community/mailing-lists/index.php
+++ files/community/mailing-lists/index.php
@@ -1,76 +1,89 @@
 <?php include "../../functions.php"; luwrain_begin_page('/community/mailing-lists/index.php', luwrain_current_lang() == 'ru'?'Списки рассылок':'Mailing lists');?>
 <?php if (luwrain_current_lang() == 'en') {?>
-                <h1>Our mailing lists</h1>
-                <p>
-                  We have four mailing lists on&#160;<a href="http://googlegroups.com">googlegroups.com</a>.
-                  For&#160;the subscribing to&#160;them send a&#160;message to&#160;the&#160;address mentioned
-                  near each option.
-                </p>
-                <h2>For&#160;English users</h2>
-                <ul>
-                  <li>Name: luwrain-users-en@googlegroups.com</li>
-                  <li>Language: English</li>
-                  <li>Description: Discussing of&#160;LUWRAIN using in&#160;English</li>
-                  <li>Subscribe: <a href="mailto:luwrain-users-en+subscribe@googlegroups.com">luwrain-users-en+subscribe@googlegroups.com</a></li>
-                </ul>
-                <h2>For&#160;English developers</h2>
-                <ul>
-                  <li>Name: luwrain-devel-en@googlegroups.com</li>
-                  <li>Language: English</li>
-                  <li>Description: Discussing of&#160;LUWRAIN developing in&#160;English</li>
-                  <li>Subscribe: <a href="mailto:luwrain-devel-en+subscribe@googlegroups.com">luwrain-devel-en+subscribe@googlegroups.com</a></li>
-                </ul>
-                <h2>For&#160;Russian users</h2>
-                <ul>
-                  <li>Name: luwrain-users-ru@googlegroups.com</li>
-                  <li>Language: Russian</li>
-                  <li>Description: Discussing of&#160;LUWRAIN using in&#160;Russian</li>
-                  <li>Subscribe: <a href="mailto:luwrain-users-ru+subscribe@googlegroups.com">luwrain-users-ru+subscribe@googlegroups.com</a></li>
-                </ul>
-                <h2>For&#160;Russian developers</h2>
-                <ul>
-                  <li>Name: luwrain-devel-ru@googlegroups.com</li>
-                  <li>Language: Russian</li>
-                  <li>Description: Discussing of&#160;LUWRAIN developing in&#160;Russian</li>
-                  <li>Subscribe: <a href="mailto:luwrain-devel-ru+subscribe@googlegroups.com">luwrain-devel-ru+subscribe@googlegroups.com</a></li>
-                </ul>
+<h1>Our mailing lists</h1>
 
+<p>We have four mailing lists on&#160;<a href="http://googlegroups.com">googlegroups.com</a>.
+To&#160;subscribe to&#160;one of&#160;them send a&#160;message to&#160;the&#160;address 
+mentioned at&#160;each of&#160;the&#160;options below.</p>
+
+<h2>For&#160;English users</h2>
+
+<ul>
+<li>Name: luwrain-users-en@googlegroups.com</li>
+<li>Language: English</li>
+<li>Description: Discussing LUWRAIN using in&#160;English</li>
+<li>Subscribe: <a href="mailto:luwrain-users-en+subscribe@googlegroups.com">luwrain-users-en+subscribe@googlegroups.com</a></li>
+</ul>
+
+<h2>For&#160;English developers</h2>
+
+<ul>
+<li>Name: luwrain-devel-en@googlegroups.com</li>
+<li>Language: English</li>
+<li>Description: Discussing LUWRAIN developing in&#160;English</li>
+<li>Subscribe: <a href="mailto:luwrain-devel-en+subscribe@googlegroups.com">luwrain-devel-en+subscribe@googlegroups.com</a></li>
+</ul>
+
+<h2>For&#160;Russian users</h2>
+
+<ul>
+<li>Name: luwrain-users-ru@googlegroups.com</li>
+<li>Language: Russian</li>
+<li>Description: Discussing LUWRAIN using in&#160;Russian</li>
+<li>Subscribe: <a href="mailto:luwrain-users-ru+subscribe@googlegroups.com">luwrain-users-ru+subscribe@googlegroups.com</a></li>
+</ul>
+
+<h2>For&#160;Russian developers</h2>
+
+<ul>
+<li>Name: luwrain-devel-ru@googlegroups.com</li>
+<li>Language: Russian</li>
+<li>Description: Discussing LUWRAIN developing in&#160;Russian</li>
+<li>Subscribe: <a href="mailto:luwrain-devel-ru+subscribe@googlegroups.com">luwrain-devel-ru+subscribe@googlegroups.com</a></li>
+</ul>
 <?php }?>
 <?php if (luwrain_current_lang() == 'ru') {?>
-                <h1>Наши списки рассылок</h1>
-                <p>
-                  Обсуждение нашего проекта ведётся в&#160;четырёх списках рассылок на&#160;сервере <a href="http://googlegroups.com">googlegroups.com</a>.
-                  Все желающие подписаться приглашаются присоединиться к обсуждениям, для&#160;этого просто
-                  отправьте письмо на&#160;адрес, указанный рядом с&#160;каждым вариантом в&#160;списке ниже.
-                </p>
-                <h2>Для русскоязычных пользователей</h2>
-                <ul>
-                  <li>Название: luwrain-users-ru@googlegroups.com</li>
-                  <li>Язык: русский</li>
-                  <li>Описание: обсуждение вопросов использования LUWRAIN на&#160;русском языке</li>
-                  <li>Адрес для&#160;подписки: <a href="mailto:luwrain-users-ru+subscribe@googlegroups.com">luwrain-users-ru+subscribe@googlegroups.com</a></li>
-                </ul>
-                <h2>Для русскоязычных разработчиков</h2>
-                <ul>
-                  <li>Название: luwrain-devel-ru@googlegroups.com</li>
-                  <li>Язык: русский</li>
-                  <li>Описание: обсуждение вопросов разработки LUWRAIN на&#160;русском языке</li>
-                  <li>Адрес для&#160;подписки: <a href="mailto:luwrain-devel-ru+subscribe@googlegroups.com">luwrain-devel-ru+subscribe@googlegroups.com</a></li>
-                </ul>
-                <h2>Для&#160;англоязычных пользователей</h2>
-                <ul>
-                  <li>Название: luwrain-users-en@googlegroups.com</li>
-                  <li>Язык: английский</li>
-                  <li>Описание: обсуждение вопросов использования LUWRAIN на&#160;английском языке</li>
-                  <li>Адрес для&#160;подписки: <a href="mailto:luwrain-users-en+subscribe@googlegroups.com">luwrain-users-en+subscribe@googlegroups.com</a></li>
-                </ul>
-                <h2>Для&#160;англоязычных разработчиков</h2>
-                <ul>
-                  <li>Название: luwrain-devel-en@googlegroups.com</li>
-                  <li>Язык: английский</li>
-                  <li>Описание: обсуждение вопросов разработки LUWRAIN на&#160;английском языке</li>
-                  <li>Адрес для&#160;подписки: <a href="mailto:luwrain-devel-en+subscribe@googlegroups.com">luwrain-devel-en+subscribe@googlegroups.com</a></li>
-                </ul>
+<h1>Наши списки рассылок</h1>
 
+<p>Обсуждение нашего проекта ведётся в&#160;четырёх списках рассылок на&#160;сервере <a href="http://googlegroups.com">googlegroups.com</a>.
+Все желающие подписаться приглашаются присоединиться к обсуждениям,
+для&#160;этого просто отправьте письмо
+на&#160;адрес, указанный рядом с&#160;каждым вариантом в&#160;списке ниже.</p>
+
+<h2>Для русскоязычных пользователей</h2>
+
+<ul>
+<li>Название: luwrain-users-ru@googlegroups.com</li>
+<li>Язык: русский</li>
+<li>Описание: обсуждение вопросов использования LUWRAIN на&#160;русском языке</li>
+<li>Адрес для&#160;подписки: <a href="mailto:luwrain-users-ru+subscribe@googlegroups.com">luwrain-users-ru+subscribe@googlegroups.com</a></li>
+</ul>
+
+<h2>Для русскоязычных разработчиков</h2>
+
+<ul>
+<li>Название: luwrain-devel-ru@googlegroups.com</li>
+<li>Язык: русский</li>
+<li>Описание: обсуждение вопросов разработки  LUWRAIN на&#160;русском языке</li>
+<li>Адрес для&#160;подписки: <a href="mailto:luwrain-devel-ru+subscribe@googlegroups.com">luwrain-devel-ru+subscribe@googlegroups.com</a></li>
+</ul>
+
+<h2>Для&#160;англоязычных пользователей</h2>
+
+<ul>
+<li>Название: luwrain-users-en@googlegroups.com</li>
+<li>Язык: английский</li>
+<li>Описание: обсуждение вопросов использования LUWRAIN на&#160;английском языке</li>
+<li>Адрес для&#160;подписки: <a href="mailto:luwrain-users-en+subscribe@googlegroups.com">luwrain-users-en+subscribe@googlegroups.com</a></li>
+</ul>
+
+<h2>Для&#160;англоязычных разработчиков</h2>
+
+<ul>
+<li>Название: luwrain-devel-en@googlegroups.com</li>
+<li>Язык: английский </li>
+<li>Описание: обсуждение вопросов разработки  LUWRAIN на&#160;английском  языке</li>
+<li>Адрес для&#160;подписки: <a href="mailto:luwrain-devel-en+subscribe@googlegroups.com">luwrain-devel-en+subscribe@googlegroups.com</a></li>
+</ul>
 <?php }?>
 <?php luwrain_end_page('/community/mailing-lists/index.php');?>
---
+++ texts/comm.mailing-lists.md
@@ -0,0 +1,77 @@
+
+ENTITLE='Mailing lists'
+RUTITLE='Списки рассылок'
+TARGET=community/mailing-lists/index.php
+
+luwrain-page-en
+
+# Our mailing lists
+
+We have four mailing lists on~[googlegroups.com](http://googlegroups.com).
+To~subscribe to~one of~them send a~message to~the~address 
+mentioned at~each of~the~options below.
+
+## For~English users
+
+* Name: luwrain-users-en@googlegroups.com
+* Language: English
+* Description: Discussing LUWRAIN using in~English
+* Subscribe: [luwrain-users-en+subscribe@googlegroups.com](mailto:luwrain-users-en+subscribe@googlegroups.com )
+
+## For~English developers
+
+* Name: luwrain-devel-en@googlegroups.com
+* Language: English
+* Description: Discussing LUWRAIN developing in~English
+* Subscribe: [luwrain-devel-en+subscribe@googlegroups.com](mailto:luwrain-devel-en+subscribe@googlegroups.com)
+
+## For~Russian users
+
+* Name: luwrain-users-ru@googlegroups.com
+* Language: Russian
+* Description: Discussing LUWRAIN using in~Russian
+* Subscribe: [luwrain-users-ru+subscribe@googlegroups.com](mailto:luwrain-users-ru+subscribe@googlegroups.com)
+
+## For~Russian developers
+
+* Name: luwrain-devel-ru@googlegroups.com
+* Language: Russian
+* Description: Discussing LUWRAIN developing in~Russian
+* Subscribe: [luwrain-devel-ru+subscribe@googlegroups.com](mailto:luwrain-devel-ru+subscribe@googlegroups.com)
+
+luwrain-page-ru
+
+# Наши списки рассылок
+
+Обсуждение нашего проекта ведётся в~четырёх списках рассылок на~сервере [googlegroups.com](http://googlegroups.com).
+Все желающие подписаться приглашаются присоединиться к обсуждениям,
+для~этого просто отправьте письмо
+на~адрес, указанный рядом с~каждым вариантом в~списке ниже.
+
+## Для русскоязычных пользователей
+
+* Название: luwrain-users-ru@googlegroups.com
+* Язык: русский
+* Описание: обсуждение вопросов использования LUWRAIN на~русском языке
+* Адрес для~подписки: [luwrain-users-ru+subscribe@googlegroups.com](mailto:luwrain-users-ru+subscribe@googlegroups.com)
+
+## Для русскоязычных разработчиков
+
+* Название: luwrain-devel-ru@googlegroups.com
+* Язык: русский
+* Описание: обсуждение вопросов разработки  LUWRAIN на~русском языке
+* Адрес для~подписки: [luwrain-devel-ru+subscribe@googlegroups.com](mailto:luwrain-devel-ru+subscribe@googlegroups.com)
+
+## Для~англоязычных пользователей 
+
+* Название: luwrain-users-en@googlegroups.com
+* Язык: английский
+* Описание: обсуждение вопросов использования LUWRAIN на~английском языке
+* Адрес для~подписки: [luwrain-users-en+subscribe@googlegroups.com](mailto:luwrain-users-en+subscribe@googlegroups.com)
+
+## Для~англоязычных разработчиков
+
+* Название: luwrain-devel-en@googlegroups.com
+* Язык: английский 
+* Описание: обсуждение вопросов разработки  LUWRAIN на~английском  языке
+* Адрес для~подписки: [luwrain-devel-en+subscribe@googlegroups.com](mailto:luwrain-devel-en+subscribe@googlegroups.com)
