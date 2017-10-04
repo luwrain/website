546d3f57608f6d19ea685af238559c5e49245934
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2017-10-05 04:29:43 +0700

    Deleting needless pages in files/

--- files/community/extensions/index.php
+++
@@ -1,3 +0,0 @@
-<?php include "../../functions.php"; luwrain_begin_page('/community/extensions/index.php', luwrain_current_lang() == 'ru'?'Расширения':'Extensions');?>
-<?php luwrain_content_not_written();?>
-<?php luwrain_end_page('/community/extensions/index.php');?>
--- files/doc/devel/design/index.php
+++
@@ -1,3 +0,0 @@
-<?php include "../../../functions.php"; luwrain_begin_page('/doc/devel/design/index.php', luwrain_current_lang() == 'ru'?'Основы дизайна Luwrain?':'Design fundamentals of Luwrain distributions');?>
-<?php luwrain_content_not_written();?>
-<?php luwrain_end_page('/doc/devel/design/index.php');?>
