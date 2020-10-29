f41de0109640d9305247e0ffb205d14af964fc6a
Michael Pozhidaev <msp@luwrain.org>
2020-10-29 11:24:28 +0700

    Updating download/iso

--- files/download/iso/writing/index.php
+++
@@ -1,8 +0,0 @@
-<?php include "../../../functions.php"; luwrain_begin_page('/download/iso/writing/index.php', luwrain_current_lang() == 'ru'?'Запись ISO-образа':'Writing the ISO-image');?>
-<?php if (luwrain_current_lang() == 'en') {?>
-<h1>Writing the&#160;ISO-image</h1>
-<?php }?>
-<?php if (luwrain_current_lang() == 'ru') {?>
-<h1>Запись ISO-образа</h1>
-<?php }?>
-<?php luwrain_end_page('/download/iso/writing/index.php');?>
